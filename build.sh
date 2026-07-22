c8c66338ab56daa8b36069639d57406879d041c8
iman130z <mollazahiahmadreza@gmail.com>
2026-07-23 00:10:19 +0430

    repair build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+echo "from django.contrib.auth import get_user_model; user = get_user_model(); user.objects.create_superuser('sina', 'sinakhammar@gmail.com', 'sina1234')" | python manage.py shell
